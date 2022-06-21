6812ad5f5eecc2ccd60c83cdbefb288a2787add2
Davy Sz <davydesouzabar@gmail.com>
2022-06-21 15:52:08 -0400

    refactor: formatting pattern

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -1,35 +1,38 @@
-import React from 'react';
+import React from "react";
+import { categories } from "../../utils/categories";
 import {
-    Container,
-    Title,
-    Amount,
-    Footer,
-    Category,
-    Icon,
-    CategoryName,
-    Date
- } from './styles';
-import {TransactionCardProps} from './types';
+  Container,
+  Title,
+  Amount,
+  Footer,
+  Category,
+  Icon,
+  CategoryName,
+  Date,
+} from "./styles";
+import { TransactionCardProps } from "./types";
 
 interface Props {
-    data: TransactionCardProps; 
+  data: TransactionCardProps;
 }
 
-export function TransactionCard({data}:Props) {
+export function TransactionCard({ data }: Props) {
+  const [category] = categories.filter((item) => item.key === data.category);
+
   return (
     <Container>
-        <Title>{data.title}</Title>
-        <Amount type={data.type}>
-        {data.type === 'negative' && '- '}
+      <Title>{data.name}</Title>
+      <Amount type={data.type}>
+        {data.type === "negative" && "- "}
         {data.amount}
-        </Amount>
-        <Footer>
-            <Category>
-                <Icon name={data.category.icon}/>
-                <CategoryName>{data.category.name}</CategoryName>
-            </Category>
-            <Date>{data.date}</Date>
-        </Footer>
+      </Amount>
+      <Footer>
+        <Category>
+          <Icon name={category.icon} />
+          <CategoryName>{category.name}</CategoryName>
+        </Category>
+        <Date>{data.date}</Date>
+      </Footer>
     </Container>
   );
 }
