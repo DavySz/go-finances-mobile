937f7d1b0b948aab78077dd5b33e2a978af07390
Davy Sz <davydesouzabar@gmail.com>
2022-06-23 13:02:32 -0400

    feat: create signin screen

--- App.tsx
+++ App.tsx
@@ -12,6 +12,8 @@ import {
 
 import theme from "./src/global/styles/theme";
 import { AppRoutes } from "./src/routes/app.routes";
+import { SignIn } from "./src/screens/SignIn";
+import { AuthProvider } from "./src/hooks/auth";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -28,7 +30,9 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content" />
-        <AppRoutes />
+        <AuthProvider>
+          <SignIn />
+        </AuthProvider>
       </NavigationContainer>
     </ThemeProvider>
   );
