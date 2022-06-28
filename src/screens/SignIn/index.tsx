b93ded2a313d9d2d9679cc6d309719384317ffa0
davy <davydesouzabar@gmail.com>
2022-06-28 15:50:46 -0400

    feat: create google and apple signin

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -19,7 +19,7 @@ import { SignInSocialButton } from "../../components/SignInSocialButton";
 import { Alert } from "react-native";
 
 export function SignIn() {
-  const { signInWithGoogle } = useAuth();
+  const { signInWithGoogle, signInWithApple } = useAuth();
 
   async function handleSignInWithGoogle() {
     try {
@@ -27,6 +27,15 @@ export function SignIn() {
     } catch (error) {
       console.log(error);
       Alert.alert("Não foi possível conectar a conta google");
+    }
+  }
+
+  async function handleSignInWithApple() {
+    try {
+      await signInWithApple();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Não foi possível conectar a conta Apple");
     }
   }
 
@@ -50,7 +59,11 @@ export function SignIn() {
             title="Entrar com o Google"
             onPress={handleSignInWithGoogle}
           />
-          <SignInSocialButton svg={AppleSvg} title="Entrar com Apple" />
+          <SignInSocialButton
+            svg={AppleSvg}
+            title="Entrar com Apple"
+            onPress={handleSignInWithApple}
+          />
         </FooterWrapper>
       </Footer>
     </Container>
