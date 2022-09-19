8031e33623bd5fda79b6e23b115dbb97cff7762e
davy <davydesouzabar@gmail.com>
2022-09-18 21:51:31 -0400

    feat: active prop

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import {TextInputProps} from 'react-native';
-import { Container } from './styles';
+import { TextInputProps } from "react-native";
+import { Container } from "./styles";
 
-type Props = TextInputProps
+interface IInput extends TextInputProps {
+  active?: boolean;
+}
 
-export function Input({...rest}:Props) {
-  return (
-    <Container {...rest}/>
-  );
+export function Input({ active = false, ...rest }: IInput) {
+  return <Container {...rest} active={active} />;
 }
